69fbb572988b870a7a241d0306f8bf0d2e371291
Anmar <anmarma83@gmail.com>
2021-02-02 13:46:40 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,9 +1,13 @@
-var greeting = "";
+/*global concatenatedString, describe, interpolatedString, it, myString */
 
-var specialGuest = "Neil deGrasse Tyson"
+it('defines `greeting`', function() {
+  expect(greeting).toEqual("Hello, everybody!")
+})
 
-var greetSpecialGuest = greeting + specialGuest + "!";
+it('concatenates strings to greet a special guest in `greetSpecialGuest`', function() {
+  expect(greetSpecialGuest).toEqual(`Hello, ${specialGuest}!`)
+})
 
-var topic = "space";
-
-var conversation = `${topic}`;
+it('interpolates a string in `conversation`', function() {
+  expect(conversation).toEqual(`Let's talk about ${topic}.`)
+})
